d0fa2ca8898ef8f1f07618584c606f1fab5b094e
jumokeoladimeji <jumoke5ng@yahoo.com>
2021-04-17 13:18:55 +0100

    chore add db config

---
+++ database/config.js
@@ -0,0 +1,63 @@
+const pg = require('pg');
+
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+pool.on('connect', () => {
+  console.log('connected to the Database');
+});
+
+/**
+ * Create Tables
+ */
+const createTables = () => {
+  const queryText =
+    `CREATE TABLE IF NOT EXISTS
+      users(
+        id INTEGER PRIMARY KEY,
+        first_name VARCHAR(15) NOT NULL,
+        last_name VARCHAR(128) NOT NULL,
+        avatar VARCHAR(128) NOT NULL,
+        created_at TIMESTAMP
+      )`;
+
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+/**
+ * Drop Tables
+ */
+const dropTables = () => {
+  const queryText = 'DROP TABLE IF EXISTS users';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+module.exports = {
+  createTables,
+  dropTables
+};
+
+require('make-runnable');
+
+module.exports = pool;
